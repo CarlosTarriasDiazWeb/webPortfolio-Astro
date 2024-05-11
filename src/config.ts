2ef8ca54f76f6cb74f012051414cd41808e551a1
carlos.tarrias.7e6 <carlos.tarrias.7e6@itb.cat>
2024-05-11 20:29:46 +0200

    Added education and svg icons

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Carlos Tarrias | Web Portfolio";
-export const SITE_DESCRIPTION = "Portfolio ";
+export const SITE_DESCRIPTION = "Web Development Portfolio Website";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
